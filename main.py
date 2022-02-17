0d7c29673dd0212283b1f3213ecc34ac51782bb4
Yurii Zinchuk <yurii.zinchuk@ucu.edu.ua>
2022-02-18 01:43:52 +0200

    documentation and look fixes

--- main.py
+++ main.py
@@ -2,14 +2,10 @@
 of people that certain account is following.
 """
 
-from crypt import methods
 import requests
 import folium as fl
 from geopy.geocoders import Nominatim
 from flask import Flask, redirect, render_template, request, url_for
-
-
-app = Flask(__name__)
 
 
 AUTH_INFO = {'Authorization': 'Bearer \
@@ -17,23 +13,45 @@ AAAAAAAAAAAAAAAAAAAAAHYZZQEAAAAACaTY6qZpAmIQumkIXGSXh%2BmermQ\
 %3Diu3as5a5RqUZ5m49HRqaGjm6dpnkXphGAVGuA82gyVlQdeWyVJ'}
 
 
+app = Flask(__name__)
+
+
 @app.route('/', methods=('GET', 'POST'))
 def enter_username():
+    """Function for handling operations with the main page.
+    Gets requests from HTML forms and provides main with args to find people.
+    Renders webpages for flask.
+
+    Returns:
+        _type_: Rendered web page.
+    """
     if request.method == 'POST':
+
         if 'show' in request.form.keys():
             return redirect(url_for('show_map'))
         else:
             username = request.form['username']
             num_of_friends = int(request.form['num_of_friends'])
+
             main(username, num_of_friends)
+
             return redirect(url_for('enter_username'))
+
     return render_template('index.html')
 
 
 @app.route('/map/', methods=('GET', 'POST'))
 def show_map():
+    """Function for handling operations with the big map page.
+    Gets requests from HTML form on the big map page and reacts on this.
+    Renders big map page and handles return to main page.
+
+    Returns:
+        _type_: Rendered web page.
+    """
     if request.method == 'POST':
         return redirect(url_for('enter_username'))
+
     return render_template('bigmap.html')
 
 
@@ -56,7 +74,7 @@ def get_id_by_username(username: str) -> str:
 
 def get_accounts_info(id: str) -> list:
     """Return a nested list of lists of names, locations
-    of people that account with this id follows.
+    of people that account with this ID follows.
 
     Args:
         id (str): ID of the account to find followers.
@@ -95,18 +113,21 @@ def get_coordinates(location: str) -> tuple:
         geocode = geocoder.geocode(location)
         lat = geocode.latitude
         lng = geocode.longitude
+
         return lat, lng
+
     except Exception:
         return None
 
 
-def get_map_info(following_info: list, num_of_friends) -> list:
+def get_map_info(following_info: list, num_of_friends: int) -> list:
     """Return list with sublists with name, location,
     and coordinates of each account, if geopy managed
     to find coordinates.
 
     Args:
         following_info (list): List with names and locations.
+        num_of_friends (int): Number of friends to show on the map.
 
     Returns:
         list: List with names, locations, and coordinates.
@@ -117,6 +138,7 @@ def get_map_info(following_info: list, num_of_friends) -> list:
     found = 0
     for index, location in enumerate(names_locations_coordinates):
         coordinates = get_coordinates(location[1])
+
         if coordinates:
             names_locations_coordinates[index].append(coordinates)
             found += 1
@@ -129,7 +151,7 @@ def get_map_info(following_info: list, num_of_friends) -> list:
     return names_locations_coordinates
 
 
-def create_map(map_info: list, num_of_friends: int):
+def create_map(map_info: list):
     """Create map based on input people's info.
 
     Args:
@@ -144,6 +166,7 @@ def create_map(map_info: list, num_of_friends: int):
     )
 
     fg_m = fl.FeatureGroup(name='People following')
+
     people = dict()
     for name, location, coordinates in map_info:
         if coordinates in people:
@@ -160,6 +183,7 @@ def create_map(map_info: list, num_of_friends: int):
 
     my_map.add_child(fg_m)
     my_map.add_child(fl.LayerControl())
+
     my_map.save('templates/MyMap.html')
     my_map.save('static/MyMap.html')
 
@@ -170,13 +194,14 @@ def main(username: str, num_of_friends: int):
 
     Args:
         username (str): Username of the account, whose
-        followers to show on the map.
+            followers to show on the map.
+        num_of_friends (int): Number of friends to show on the map.
     """
     user_id = get_id_by_username(username)
     following_info = get_accounts_info(user_id)
     map_info = get_map_info(following_info, num_of_friends)
-    create_map(map_info, num_of_friends)
+    create_map(map_info)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
